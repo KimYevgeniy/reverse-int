d7926a56724c0d571eda6c45fed70784b0e7ee9f
Yevgeniy Kim <kimeval@gmail.com>
2023-07-31 11:04:58 +0600

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const ans = Math.abs(n).toString();
+    let answer = "";
+    for (let i = ans.length - 1; i >= 0; i--) {
+        answer = answer + ans[i];
+    }
+    return parseInt(answer);
+};
